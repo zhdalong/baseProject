9c07af12a88338f8cf02562dab081527103ca1db
zhangdalong <zhangdalong@ddshenbian.com>
2018-04-09 17:04:14 +0800

    1111

--- src/main/java/cn/sn/baseProject/web/Test.java
+++ src/main/java/cn/sn/baseProject/web/Test.java
@@ -18,6 +18,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(false);
         System.out.println(false);
+        System.out.println(false);
     }
 
 
