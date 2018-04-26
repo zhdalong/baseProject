bf25482be01b9515d8d29458931b63a28361b10b
zhdalong <89922688@qq.com>
2018-04-26 21:49:36 +0800

    Update Test.java

--- src/main/java/cn/sn/baseProject/web/Test.java
+++ src/main/java/cn/sn/baseProject/web/Test.java
@@ -16,9 +16,11 @@ public class Test {
     }
 
     public static void main(String[] args) {
+        System.out.println("aa");
         System.out.println(false);
         System.out.println(false);
         System.out.println(false);
+        System.out.println("bb");
     }
 
 
